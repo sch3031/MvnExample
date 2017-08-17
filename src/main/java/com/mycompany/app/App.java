5d15ea6e6bad61f86e44a05377ec7d23a23e4b4f
Thomas McQuitty <thomas.mcquitty@ca.com>
2017-08-17 11:48:59 -0500

    US122 DE30 another change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " hasdeHello World! 21aa another test a" );
+        System.out.println( " hasdeHello World! 21aa tracking another change" );
     }
 }
 //Another comment
