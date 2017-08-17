21d823a2689b14f93d98db28ff8b78b7335dfec6
Thomas McQuitty <thomas.mcquitty@ca.com>
2017-08-17 10:12:53 -0500

    US122

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " hasdeHello World! 21aa another test" );
+        System.out.println( " hasdeHello World! 21aa another test ads" );
     }
 }
 //Comment a serious add on to functionality
