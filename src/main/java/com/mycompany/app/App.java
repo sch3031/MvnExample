93ed61f358ee2217e78e3a451d944958a797d017
Thomas McQuitty <thomas.mcquitty@ca.com>
2017-08-17 09:54:11 -0500

    US121

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " hasdeHello World! 21a another test" );
+        System.out.println( " hasdeHello World! 21aa another test" );
     }
 }
