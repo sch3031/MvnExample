f9a60c6c3850171cbc7a312633343f03c0a76ca5
chipset <thomas@mcquitty.net>
2017-08-30 12:41:56 -0500

    DE30 Testing
    Testing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( " hasdeHello World! 21aa tracking another change" );  //We really need more  value in this file
-     }
+     } // testing
 }
