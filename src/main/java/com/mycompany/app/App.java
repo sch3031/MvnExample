cb78fa8a97533858fe6eb11aa51100c3215d7c64
chipset <thomas@mcquitty.net>
2017-08-31 13:52:44 -0500

    US124 DE31 Some simple changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! 21aa tracking another change" );  //We really need more  value in this file
-     } //system what an update!!
+     } 
+     
 }
