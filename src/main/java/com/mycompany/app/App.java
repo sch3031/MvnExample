8e7edd5d4dc2ec405c48f236a2f2526d2f163dcb
chipset <thomas@mcquitty.net>
2017-09-01 08:45:24 -0500

    US31 New Change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! 21aa tracking another change" );  //We really need more  value in this file
-     } 
+     } //This is a change on 9/1
      
 }
