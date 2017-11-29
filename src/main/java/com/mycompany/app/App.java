0c567f8693bd20544e028fa69a5a737035ffcd2b
chipset <thomas@mcquitty.net>
2017-11-29 13:12:30 -0600

    US154 DE38 This is a change for fixes and demprep

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 21aa tracking another change" );  //We really need more  value in this file
-     } //This is a change on 9/1 for the second time  make another comment  This is a comment
-    //Test for tom     and another change.
+        System.out.println( "Hello World! 21aa tracking another change" ); 
+     } 
 }
