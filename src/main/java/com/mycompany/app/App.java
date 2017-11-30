3bd3eb371bb754d3aea35abbf1474d0bc6cb8e73
chipset <thomas@mcquitty.net>
2017-11-30 10:12:00 -0600

    US155 DE38  This is a fix for Stephanie's Problem.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 21aa tracking another change" ); 
+        for (int i = 0; i < 30; i++)
+        {
+            System.out.println( "Hello World!" ); 
+        }
      } 
 }
